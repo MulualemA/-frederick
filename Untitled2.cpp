6be4ea6f7cbd84356ddfa27b54c68f4f38096b41
1NourHany <88481011+1NourHany@users.noreply.github.com>
2021-08-05 12:30:06 +0200

    Add C++ Solution

---
+++ Untitled2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Nour Hany";
+    std::cout << " ,nourmarzouk278@gmail.com";
+    std::cout << " ,@Nour";
+    std::cout << " ,Genomics& Transcriptomics &Drug Development& Medicinal Chemistry and Cheminformatics& Vaccine Informatics\n";
+
+
+
+}
